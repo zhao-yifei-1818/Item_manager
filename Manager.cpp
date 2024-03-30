f91b63223b57da469d01fbec819c7f786633a3ab
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-30 22:11:18 +0000

    Mar 30, 2024, 3:11 PM

--- Manager.cpp
+++ Manager.cpp
@@ -1,7 +1,6 @@
 #include "Manager.h"
 #include <fstream>
 #include <iostream>
-
 void Manager::addItem(const Item& item)
 {
   items.push_back(item);
@@ -23,9 +22,28 @@ void Manager::exportToFile(const std::string& filename) const
 
   for (const auto& item : items) {
     outFile << item.getRecipient() << " " << item.getValue() << " "
-            << item.getDate() << " " << item.getCostToShip() << std::endl;
+            << item.getDate() << " " << item.getCostToShip() << " "
+            << item.getReward() << std::endl;
   }
 
   outFile.close();
   std::cout << "Item details exported to " << filename << std::endl;
+}
+
+void Manager::readFromFile(const std::string& filename)
+{
+  std::ifstream inFile(filename);
+  if (!inFile) {
+    std::cerr << "Error opening file: " << filename << std::endl;
+    return;
+  }
+
+  std::string recipient;
+  int value, date, costToShip;
+  while (inFile >> recipient >> value >> date >> costToShip) {
+    addItem(Item(recipient, value, date, costToShip));
+  }
+
+  inFile.close();
+  std::cout << "Item details read from " << filename << std::endl;
 }
