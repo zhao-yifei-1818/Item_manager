d906285449a1029242f5560f02caaef4b70660b5
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-30 03:45:14 +0000

    Mar 29, 2024, 8:45 PM

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "Manager.h"
 #include "Item.h"
+#include "Manager.h"
 #include <iostream>
 #include <sstream>
 
@@ -7,37 +7,35 @@ int main()
 {
   Manager manager;
   std::string line;
-  int selection = 0;
-  while(selection != 0){
-    std::cout << "Input an Action number:\n1: Add Item\n2: Load from Item.txt3: printAll()4: Export to Item.txt\nInput: "<< std::endl;
-    if (selection == 1){
-  std::cout << "Enter item details (recipient, value, date, cost to ship) "
-               "separated by spaces: ";
-  std::getline(std::cin, line);
+  int selection = 5;
+  while (selection != 0) {
+    std::cout << "Input an Action number:\n1: Add Item\n2: Load from "
+                 "Item.txt3: printAll()4: Export to Item.txt\nInput: "
+              << std::endl;
+    if (selection == 1) {
+      std::cout << "Enter item details (recipient, value, date, cost to ship) "
+                   "separated by spaces: ";
+      std::getline(std::cin, line);
 
-  std::stringstream ss(line);
-  std::string recipient;
-  int value, date, costToShip;
+      std::stringstream ss(line);
+      std::string recipient;
+      int value, date, costToShip;
 
-  ss >> recipient >> value >> date >> costToShip;
+      ss >> recipient >> value >> date >> costToShip;
 
-  Item newItem(recipient, value, date, costToShip);
-  manager.addItem(newItem);
+      Item newItem(recipient, value, date, costToShip);
+      manager.addItem(newItem);
 
-  std::cout << "Added item details:\n";
-  manager.printAllItems();
-  // Export item details to a text file
-    }
-    else if (selection == 2){
+      std::cout << "Added item details:\n";
+      manager.printAllItems();
+      // Export item details to a text file
+    } else if (selection == 2) {
       manager.readFromFile("items.txt");
-    }
-    else if (selection == 3){
-    manager.printAllItems();
+    } else if (selection == 3) {
+      manager.printAllItems();
 
-    }
-    else if (selection == 4){
-    manager.exportToFile("items.txt");
-
+    } else if (selection == 4) {
+      manager.exportToFile("items.txt");
     }
   }
 
